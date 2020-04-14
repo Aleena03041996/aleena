65a065fc80347d2a56c8caa75ab90ff1452afa5a
Aleena03041996 <63460643+Aleena03041996@users.noreply.github.com>
2020-04-14 16:47:15 +0530

    uploading my project to repository

---
+++ java_training/src/main/java/java_training/Sample.java
@@ -0,0 +1,16 @@
+package java_training;
+
+public class Sample {
+	 public static void main(String args[])
+	 {  
+	    int a=2;
+	    int b=3;
+	    int c;
+	    c=a+b;
+	    System.out.println("Sum is"+c);
+	  }
+
+}
+
+
+
